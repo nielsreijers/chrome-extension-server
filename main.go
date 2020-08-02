688569f6e9fe7eedb1353c954428657a4e549972
Niels Reijers <nielsreijers@gmail.com>
2020-08-02 11:49:26 +0800

    Simple proxy for cofacts

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/russross/blackfriday"
 )
+
+const cofactsGqlQuery = `
+query($text: String) {
+  ListArticles(
+	filter: { moreLikeThis: { like: $text } }
+	orderBy: [{ _score: DESC }]
+	first: 4
+  ) {
+	edges {
+	  node {
+		id
+		text
+		hyperlinks {
+		  url
+		}
+		articleReplies {
+		  reply {
+			id
+			text
+			type
+			reference
+		  }
+		}
+	  }
+	}
+  }
+}`
 
 func main() {
 	port := os.Getenv("PORT")
@@ -28,6 +58,32 @@ func main() {
 
 	router.GET("/mark", func(c *gin.Context) {
 		c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
+	})
+
+	router.GET("/cofacts", func(c *gin.Context) {
+		type CofactsRequestVariables struct {
+			Text string `json:"text"`
+		}
+
+		type CofactsRequest struct {
+			Query     string                  `json:"query"`
+			Variables CofactsRequestVariables `json:"variables"`
+		}
+
+		text := c.DefaultQuery("text", "")
+
+		cofactsQuery := CofactsRequest{
+			Query:     cofactsGqlQuery,
+			Variables: CofactsRequestVariables{Text: text},
+		}
+		body, _ := json.Marshal(&cofactsQuery)
+
+		resp, _ := http.Post("https://cofacts-api.g0v.tw/graphql", "application/json", strings.NewReader(string(body)))
+
+		defer resp.Body.Close()
+		respText, _ := ioutil.ReadAll(resp.Body)
+
+		c.String(http.StatusOK, string(respText))
 	})
 
 	router.Run(":" + port)
