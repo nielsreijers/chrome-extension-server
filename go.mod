545bdd0428e4bf175f7a49198db4994144cbbe68
Niels Reijers <nielsreijers@gmail.com>
2020-08-04 11:15:17 +0800

    Added IsMatch attribute to Cofacts data to indicate if the article found through Cofacts really matches the search query. For debugging we still return all found articles. Later we should only return the best match.

--- go.mod
+++ go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
 	github.com/russross/blackfriday v2.0.0+incompatible
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	gopkg.in/vmarkovtsev/go-lcss.v1 v1.0.0-20181020221121-dfc501d07ea0
+	mvdan.cc/xurls/v2 v2.2.0
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
@@ -12,6 +13,8 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/russross/blackfriday"
+	"gopkg.in/vmarkovtsev/go-lcss.v1"
+	"mvdan.cc/xurls/v2"
 )
 
 const cofactsGqlQuery = `
@@ -41,6 +44,7 @@ query($text: String) {
   }
 }`
 
+// TODO: get the whole Schema from https://cofacts-api.g0v.tw/graphql
 type Hyperlink struct {
 	Url string `json:"url"`
 }
@@ -52,11 +56,21 @@ type ArticleReply struct {
 	Reference string `json:"reference"`
 }
 
+type ArticleReplies struct {
+	Reply ArticleReply `json:"reply"`
+}
+
 type Node struct {
-	Id             string         `json:"id"`
-	Text           string         `json:"text"`
-	Hyperlinks     []Hyperlink    `json:"hyperlinks"`
-	ArticleReplies []ArticleReply `json:"articleReplies"`
+	Id             string           `json:"id"`
+	Text           string           `json:"text"`
+	Hyperlinks     []Hyperlink      `json:"hyperlinks"`
+	ArticleReplies []ArticleReplies `json:"articleReplies"`
+
+	// Added by this server to indicate whether the article matches the search query.
+	// We should just filter in the final version, but for development it will be
+	// useful to see what results we get from Cofacts and whether the server accepts
+	// or rejects them.
+	IsMatch bool `json:"ismatch"`
 }
 
 type Edge struct {
@@ -107,16 +121,114 @@ func main() {
 	router.Run(":" + port)
 }
 
+func isEquivalent(url1 string, url2 string) bool {
+	u1, err := url.Parse(url1)
+	if err != nil {
+		panic(err)
+	}
+	u2, err := url.Parse(url2)
+	if err != nil {
+		panic(err)
+	}
+	if u1.Host != u2.Host {
+		return false
+	}
+	if strings.TrimRight(u1.Path, "/") != strings.TrimRight(u2.Path, "/") {
+		return false
+	}
+	q1 := u1.Query()
+	q2 := u2.Query()
+	for k, vs := range q1 {
+		for _, v1 := range vs {
+			var found = false
+			for _, v2 := range q2[k] {
+				if v1 == v2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func exist_same_url(node *Node, request_urls []string) bool {
+	for _, hyperlink := range node.Hyperlinks {
+		node_url := hyperlink.Url
+		for _, request_url := range request_urls {
+			if isEquivalent(node_url, request_url) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func removeWhitespace(s string) string {
+	s = strings.ReplaceAll(s, "\n", "")
+	s = strings.ReplaceAll(s, "\r", "")
+	s = strings.ReplaceAll(s, "\t", "")
+	s = strings.ReplaceAll(s, " ", "")
+	return s
+}
+
 func handleCofacts(c *gin.Context) {
 	text := c.DefaultQuery("text", "")
 
-	respText := callCofactsApi(text)
+	// Call the Cofacts api
+	respText, err := callCofactsApi(text)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "error:", err)
+		return
+	}
+
+	// Convert to CofactResponse struct
+	var respData CofactResponse
+	err = json.Unmarshal([]byte(respText), &respData)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "error:", err)
+		return
+	}
+
+	// Follow roughly the same filter approach as Aunt Meiyu
+	rxStrict := xurls.Strict()
+	request_urls := rxStrict.FindAllString(text, -1)
+	if len(request_urls) > 0 {
+		// If there's a url in the text, it must be in the article
+		for i := range respData.Data.ListArticles.Edges {
+			node := &respData.Data.ListArticles.Edges[i].Node
+			node.IsMatch = exist_same_url(node, request_urls)
+		}
+	} else {
+		// Todo: should use tf-idf, but for an early demo this is good enough
+		for i := range respData.Data.ListArticles.Edges {
+			node := &respData.Data.ListArticles.Edges[i].Node
+
+			// strip any whitespace for comparison
+			a := removeWhitespace(text)
+			b := removeWhitespace(node.Text)
+			common := lcss.LongestCommonSubstring([]byte(a), []byte(b))
+			// Match if least 25 characters, or 80% of the query text in common
+			// TODO strip newlines
+			node.IsMatch = (len(common) > 25) || (len(common)*100/len(text) >= 80)
+		}
+	}
+
+	// Convert back to json
+	respTextBytes, err := json.Marshal(&respData)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "error:", err)
+		return
+	}
 
 	c.Header("Cache-Control", "public,max-age=86400")
-	c.String(http.StatusOK, string(respText))
+	c.String(http.StatusOK, string(respTextBytes))
 }
 
-func callCofactsApi(text string) string {
+func callCofactsApi(text string) (string, error) {
 	type CofactsRequestVariables struct {
 		Text string `json:"text"`
 	}
@@ -130,12 +242,25 @@ func callCofactsApi(text string) string {
 		Query:     cofactsGqlQuery,
 		Variables: CofactsRequestVariables{Text: text},
 	}
-	body, _ := json.Marshal(&cofactsQuery)
 
-	resp, _ := http.Post("https://cofacts-api.g0v.tw/graphql", "application/json", strings.NewReader(string(body)))
+	body, err := json.Marshal(&cofactsQuery)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(
+		"https://cofacts-api.g0v.tw/graphql",
+		"application/json",
+		strings.NewReader(string(body)))
+	if err != nil {
+		return "", err
+	}
 
 	defer resp.Body.Close()
-	respText, _ := ioutil.ReadAll(resp.Body)
+	respText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
 
-	return string(respText)
+	return string(respText), nil
 }
